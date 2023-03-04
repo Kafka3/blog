318f1434d29d56f8cb6e936105dc84514dd08198
Kafka3 <lli471426@gmail.com>
2023-03-04 13:20:33 +0800

    new

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `donghao's Blog`;
+export const SITE_DESCRIPTION = 'Every day is worthy of singing praises~';
+export const SITE_EMAIL = 'lli471426@gmail.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
