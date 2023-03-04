0330f14f1144a152622954b767c42cf0a82b84d8
Kafka3 <lli471426@gmail.com>
2023-03-04 13:43:46 +0800

    new

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `donghao's Blog`;
 export const SITE_DESCRIPTION = 'Every day is worthy of singing praises~';
 export const SITE_EMAIL = 'lli471426@gmail.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = 'blog.dongh.tech';
+export const SITE_URL = "https://blog.dongh.tech/";
